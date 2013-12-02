ebd5b9a070dd0906c5538c40922d91c446dc7ba1
Manfred Schmid <Manfred@klangobjekte.com>
2013-12-02 12:39:54 +0100

    pre

--- main.cpp
+++ main.cpp
@@ -20,7 +20,9 @@ bool eingabeBits(unsigned int dualzahl[],int count);
 
 //! Anzeige der Eingabe
 void printEingabe(unsigned int dualzahl[],unsigned int laenge);
-unsigned long berechneErgebniss(unsigned int dualzahl[],unsigned int laenge);
+
+unsigned long berechneErgebniss(unsigned int dualzahl[],unsigned int laenge,int basis = 2);
+
 void printErgebnis(unsigned long dezi);
 
 
@@ -81,19 +83,20 @@ void initBuffer(unsigned int dualzahl[],unsigned int laenge){
 }
 
 
-
+#define NEUE_EINGABE
+#ifndef NEUE_EINGABE
 bool eingabeBits(unsigned int dualzahl[],int count){
     unsigned long kuerzer =0,letzte =0;
     bool eingabeOk = true;
-    int i = 0;
-    cout << "Geben  Sie Byte "<< count << " ein: (niederwertigstes Bit zuerst)" << endl;
+
+    cout << "Geben  Sie die "<< count+1 << ".ten 8 Bit ein: (niederwertigstes Bit zuerst)" << endl;
     cin >> kuerzer;
-    do{
+    for (int i=0;i<8;i++){
         letzte = kuerzer%10;
         //cout << "letzte " << letzte << endl;
-        if(letzte < 0 || letzte > 1 || i>8 || cin.fail()){
+        if(kuerzer > 11111111 || letzte < 0 || letzte > 1 || cin.fail()){
             eingabeOk = false;
-            cout << "\nFalsche Eingabe. Die Zahl darf nur 0 oder 1 als Ziffer beinhalten und maximal 8 Stellen pro Eingabe haben!\nEingabe Wiederholen:" << endl;
+            cout << "\nFalsche Eingabe. Die Zahl darf nur 0 oder 1 als Ziffer beinhalten und max 8 Stellen haben!\nEingabe Wiederholen:" << endl;
             cin.clear();
             cin.ignore(BUFSIZ, '\n');
             for(int y = 0;y<i;i++){
@@ -102,17 +105,69 @@ bool eingabeBits(unsigned int dualzahl[],int count){
             break;
         }
         else{
+
             eingabeOk = true;
             kuerzer = kuerzer/10;
             dualzahl[i+count*8] = letzte;
-            i++;
+
         }
-    }while(letzte);
+    };
     return eingabeOk;
+#else
+bool eingabeBits(unsigned int dualzahl[],int count){
+
+        unsigned int byte[8];
+        unsigned long zahl =0;
+        unsigned int bit = 0;
+        for (int i=0;i<8;i++){
+            cout << "geben sie Bit "<< i+count*8 <<" ein:" << endl;
+            bool nochmal = false;
+            do{
+                cin >> bit;
+                if(bit == 1 || bit == 0){
+                    byte[i] = bit;
+                    nochmal = false;
+                }
+                else{
+                    cout << "\nFalsche Eingabe. Ein Bit kann nur den Wert 0 oder 1 annehmen!\nEingabe Wiederholen:" << endl;
+                    cin.clear();
+                    cin.ignore(BUFSIZ, '\n');
+                    nochmal = true;
+                }
+            }while(nochmal);
+        }
+        /*
+        for(unsigned int i= 0;i<8;i++){
+            unsigned int z = 8 - i;
+            unsigned long dezistelle = 1;
+            while(z<8){
+                dezistelle *= 10;
+                z++;
+            }
+            zahl += dezistelle*byte[i];
+        }
+        */
+        zahl = berechneErgebniss(byte,8,10);
+        //cout << "zahl: " << zahl << endl;
+        int i=0;
+        unsigned long letzte =0;
+        do{
+            letzte = zahl%10;
+            //cout << "zahl: " << zahl << " letzte: " << letzte;
+            zahl = zahl/10;
+            //cout << "  durchlauf: " << i <<endl;
+            dualzahl[i+count*8] = letzte;
+            i++;
+        }while(i<8);
+        return true;
 }
+#endif
+
+
+
 
 void printEingabe(unsigned int dualzahl[],unsigned int laenge){
-    cout << "Ihre Eingabe: " << endl;
+    cout << endl <<"Ihre Eingabe: " << endl;
     for (int i=laenge-1;i>=0;i--){
         cout << dualzahl[i];
         if (i % 4 == 0) cout << " ";
@@ -122,7 +177,11 @@ void printEingabe(unsigned int dualzahl[],unsigned int laenge){
     cout << endl;
 }
 
-unsigned long berechneErgebniss(unsigned int dualzahl[],unsigned int laenge){
+unsigned long berechneErgebniss(unsigned int dualzahl[],
+                                unsigned int laenge,
+                                int basis){
+    //#define KOMPLIZIERT
+    #ifdef KOMPLIZIERT
     unsigned long dezi = 0;
     for ( unsigned int i=0;i<laenge;i++){
         unsigned long dezistelle = 1;
@@ -132,13 +191,30 @@ unsigned long berechneErgebniss(unsigned int dualzahl[],unsigned int laenge){
         else{
              unsigned int z = laenge-i;
             while(z < laenge){
-                dezistelle = 2*dezistelle;
+                dezistelle *= 2;
                 z++;
             }
         }
-        dezi = dezi + dezistelle*dualzahl[i];
+        dezi += dezistelle*dualzahl[i];
     }
     return dezi;
+
+    #else
+    unsigned long ergebnis = 0;
+    //unsigned int n = laenge-1;
+    for(unsigned int i= 0;i<laenge;i++){
+            unsigned int z = laenge - i;
+            unsigned long dezistelle = 1;
+            while(z<laenge){
+                dezistelle *= basis;
+                z++;
+            }
+        ergebnis += dezistelle*dualzahl[i];
+    }
+    return ergebnis;
+    #endif
+
+
 }
 
 void printErgebnis(unsigned long dezi){
